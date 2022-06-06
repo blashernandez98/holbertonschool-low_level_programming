bf4e73232122960b024550e6dfc28dd1e0976686
Blas <blashernandez98@gmail.com>
2022-06-06 09:43:09 -0700

    Task 8

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0. (on success)
+ *
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 60);
+	return (0);
+}
