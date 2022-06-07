aba006b5561131d5a4434fb056a3c4ddf4231d3c
Blas <blashernandez98@gmail.com>
2022-06-07 05:42:59 -0700

    Task 1-6

---
+++ variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+	int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+		putchar(d + '0');
+	printf("\n");
+	return (0);
+}
