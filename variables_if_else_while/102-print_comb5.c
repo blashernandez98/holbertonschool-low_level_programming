733da077be0ed03e78e683ef2c46d01c8c68eb18
Blas <blashernandez98@gmail.com>
2022-06-07 09:14:15 -0700

    Task 1-102

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -9,34 +9,35 @@
 {
 	int f, s, aux;
 
-/**
- * conv - This prints two digit integers in format "00 01 ... 98 99".
- * @n: Integer to print.
- */
-	void conv(int n)
-	{
-		if (n < 10)
-		{
-			putchar('0');
-			putchar(n + '0');
-		}
-		else
-		{
-			aux = n / 10;
-			putchar(aux + '0');
-			aux = n % 10;
-			putchar(aux + '0');
-		}
-
-	}
-
 	for (f = 0; f <= 99; f++)
 	{
 		for (s = (f + 1); s <= 99; s++)
 		{
-			conv(f);
+			if (f < 10)
+			{
+				putchar('0');
+				putchar(f + '0');
+			}
+			else
+			{
+				aux = f / 10;
+				putchar(aux + '0');
+				aux = f % 10;
+				putchar(aux + '0');
+			}
 			putchar(' ');
-			conv(s);
+			if (s < 10)
+			{
+				putchar('0');
+				putchar(s + '0');
+			}
+			else
+			{
+				aux = s / 10;
+				putchar(aux + '0');
+				aux = s % 10;
+				putchar(aux + '0');
+			}
 			if (!((f == 98) && (s == 99)))
 			{
 				putchar(',');
@@ -44,7 +45,6 @@
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
