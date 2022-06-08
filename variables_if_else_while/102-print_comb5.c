10b8f1205cf6254f611f8d86c85f241d1bb9b501
Blas <blashernandez98@gmail.com>
2022-06-08 07:29:39 -0700

    Task 1-102

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -7,32 +7,18 @@
  */
 	int main(void)
 {
-	int f, s, aux, aux2, i;
+	int f, s;
 
 	for (f = 0; f <= 99; f++)
 	{
 		for (s = (f + 1); s <= 99; s++)
 		{
-			aux = f;
-			for (i = 0; i <= 1; i++)
-			{
-
-				if (aux < 10)
-				{
-					putchar('0');
-					putchar(aux + '0');
-				}
-				else
-				{
-					aux2 = aux / 10;
-					putchar(aux2 + '0');
-					aux2 = aux % 10;
-					putchar(aux2 + '0');
-				}
-				if (i == 0)
-					putchar(' ');
-				aux = s;
-			}
+			putchar((f / 10) + '0');
+			putchar((f % 10) + '0');
+			putchar(' ');
+			putchar((s / 10) + '0');
+			putchar((s % 10) + '0');
+				
 			if (!((f == 98) && (s == 99)))
 			{
 				putchar(',');
