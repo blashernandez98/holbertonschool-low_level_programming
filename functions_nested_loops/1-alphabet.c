36bb7daf19debeb2a968b12a059012af250444fb
Blas <blashernandez98@gmail.com>
2022-06-09 05:47:33 -0700

    Task 0-1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point.
+ * This program prints the alphabet in lowercase in order followed by a newline
+ *
+ * Return: Always 0 (on success).
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
