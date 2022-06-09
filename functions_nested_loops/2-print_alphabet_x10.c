c0106c2e595c733ede680b6ed79f11be967eba8f
Blas <blashernandez98@gmail.com>
2022-06-09 05:56:13 -0700

    Task 0-2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point.
+ * This program prints the alphabet in lowercase in order
+ * 10 times followed by a newline
+ *
+ * Return: Always 0 (on success).
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
