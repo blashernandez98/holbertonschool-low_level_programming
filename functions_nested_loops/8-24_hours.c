7b460b2ba57a8dd458271753aefbf2b916351020
Blas <blashernandez98@gmail.com>
2022-06-09 07:24:47 -0700

    Task 0-8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,12 +13,26 @@ void jack_bauer(void)
 		for (m = 0; m < 60; m++)
 		{
 			if (h < 10)
+			{
 				_putchar('0');
-			_putchar(h + '0');
+				_putchar(h + '0');
+			}
+			else
+			{
+				_putchar(h / 10 + '0');
+				_putchar(h % 10 + '0');
+			}
 			_putchar(':');
 			if (m < 10)
+			{
 				_putchar('0');
-			_putchar(m + '0');
+				_putchar('m' + '0');
+			}
+			else
+			{
+				_putchar(m / 10 + '0');
+				_putchar(m % 10 + '0');
+			}
 			_putchar('\n');
 		}
 	}
