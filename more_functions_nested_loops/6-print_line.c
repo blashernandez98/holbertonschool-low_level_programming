7fc9754d2fff2dfc2f38ea6648e871dbd9902008
Blas <blashernandez98@gmail.com>
2022-06-10 14:02:43 -0700

    Task 2-6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - Function that prints a line to std output.
+ * @n: Number of _ characters to print.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
