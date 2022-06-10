d1d3f2057030c172fac7ef66e9018f3efb6ca462
Blas <blashernandez98@gmail.com>
2022-06-10 15:16:56 -0700

    Task 2-10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,12 +9,14 @@ void print_triangle(int n)
 {
 	int i, j;
 
-	for (i = 1 ; i <= n; i++)
+	for (i = 1; i <= n; i++)
 	{
 		for (j = n - i; j > 0; j--)
 			_putchar(' ');
 		for (j = i; j > 0; j--)
 			_putchar('#');
 		_putchar('\n');
-	}	
+	}
+	if (n <= 0)
+	_putchar('\n');	
 }
