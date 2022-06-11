d6258d465d5a4a94ac59a6fb5e0390cc1a44e03e
Blas <blashernandez98@gmail.com>
2022-06-11 16:00:54 -0700

    Task 0-102

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, program that prints first 50 fibonacci numbers.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int first, second, third;
+	int i;
+
+	first = 1;
+	second = 2;
+	printf("1, 2");
+	for (i = 0; i < 47; i++)
+	{
+		third = first + second;
+		printf(", %ld", third);
+		first = second;
+		second = third;
+	}
+	printf("\n");
+	return (0);
+}
