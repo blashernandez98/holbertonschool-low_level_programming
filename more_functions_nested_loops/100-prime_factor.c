f45a41d9b8c91a379f81a2c797590cdf2c440a4c
Blas <blashernandez98@gmail.com>
2022-06-11 17:22:51 -0700

    Task

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * isprime - Function that evaluates a number as prime.
+ * @n: Number to evaluate.
+ *
+ * Return: 1 if @n is prime, 0 otherwise.
+ */
+
+int isprime(unsigned long int n)
+{
+	unsigned long int i;
+	for (i = n / 2; i > 3; i--)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
+}
+
+
+/**
+ * main - Entry point
+ * Program that finds and prints the largest prime
+ * factor of the number 612852475143
+ */
+
+int main(void)
+{
+	unsigned long int n = 612852475143;
+	unsigned long int i, largest;
+
+	for (i = n / 2; i > 0; i--)
+	{
+		if ((n % i == 0) && (isprime(largest) == 1))
+			largest = i;
+		printf("%ld %ld", i, largest);
+	}
+	printf("%ld\n", largest);
+	return (0);
+}
