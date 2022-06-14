d070a4d86a4229fb72bd73d682bd4664de3b6a9c
Blas <blashernandez98@gmail.com>
2022-06-14 08:22:49 -0700

    Task-0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - Function that takes a pointer to an int as parameter
  * and updates the value it points to to 98.
- * @n - Pointer to int to reset.
+ * @n: Pointer to int to reset.
  */
 
 void reset_to_98(int *n)
