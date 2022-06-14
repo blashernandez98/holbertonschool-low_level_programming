f5e8c49cb9661ba3c72c76362a4cd1488c631b1d
Blas <blashernandez98@gmail.com>
2022-06-14 09:40:53 -0700

    Task-5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that returns the length of a string.
+ * @s: String to calculate length of.
+ * Return: Lenght of string @s.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * rev_string - Function that reverses a string
+ * @s: String to reverse
+ */
+
+void rev_string(char *s)
+{
+	int length = _str
+	char *end = s + length - 1;
+	char aux;
+
+	for (i = 0; i < length / 2; i++)
+	{
+		aux = s[i];
+		*s = *end;
+		*end = aux;
+		s++;
+		end--;
+	}
+
+}
