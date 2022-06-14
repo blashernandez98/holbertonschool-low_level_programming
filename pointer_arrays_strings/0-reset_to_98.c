498f770468ece03357066bfb582dd69b15fb9230
Blas <blashernandez98@gmail.com>
2022-06-14 08:15:20 -0700

    Task-0

---
+++ pointer_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ * @n - Pointer to int to reset.
+ */
+
+void print_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointer_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# This directory contains all the challenges for the "C - Pointers, arrays, strings" project.
