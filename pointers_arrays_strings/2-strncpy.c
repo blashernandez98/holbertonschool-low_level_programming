8e788f92c42c8e4444a4243a80d81b4efbccbb19
Blas <blashernandez98@gmail.com>
2022-06-16 07:26:33 -0700

    Task-2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strncpy - Copies a string.
@@ -12,12 +13,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	printf("dest: %s\nsrc: %s\n", dest, src);
+
+	for (i = 0; i < n && src[i]; i++)
 	{
-		if (src)
-			dest[i] = src[i];
-		dest[i] = '\0';
+		dest[i] = src[i];
 	}
+	for (; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
