988e652a8f1bedbe563100bb22d220922c294abc
Blas <blashernandez98@gmail.com>
2022-06-16 08:12:21 -0700

    Task-3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - Same to strcmp.
+ * @s1: First string.
+ * @s2: Second string.
+ * Return: Int value.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int res = 0;
+	
+	if (s1 && s2)
+	{
+		while (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		res = (int)*s1 - (int)*s2;
+	}
+	return (res)
+}
