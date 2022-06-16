70eba90dd06d444beeb2120bf8c80471dea57809
Blas <blashernandez98@gmail.com>
2022-06-16 05:19:18 -0700

    Task-2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -16,5 +16,12 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 	}
+	if (i < n)
+	{
+		for (; dest[i]; i++)
+			dest[i] = '\0';
+	}
+	else
+		dest[i] = '\0';
 	return (dest);
 }
