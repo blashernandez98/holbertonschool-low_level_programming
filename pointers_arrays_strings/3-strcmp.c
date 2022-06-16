590827495cddd2e11dbb1f27b4039631e09053b3
Blas <blashernandez98@gmail.com>
2022-06-16 09:12:44 -0700

    Task-3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcmp - Same to strcmp.
@@ -11,12 +12,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int res = 0;
 	
-	if (s1 && s2)
+	if (*s1 && *s2)
 	{
-		while (*s1 == *s2 && s1)
+		while ((*s1 == *s2) && (*s1 && *s2))
 		{
-			s1++;
-			s2++;
+			if (*s1 && *s2)
+			{
+				s1++;
+				s2++;
+			}
 		}
 		res = (int)*s1 - (int)*s2;
 	}
