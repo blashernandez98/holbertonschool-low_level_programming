1f9f788f22082d1ba424b5c1005dc11263e8b73d
Blas <blashernandez98@gmail.com>
2022-06-16 11:41:30 -0700

    Task-5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Converts all lowercase to uppercase.
+ * @s: String to convert.
+ * Return: Pointer to string.
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	if (s)
+	{
+		for (i = 0; s[i]; i++)
+		{
+			if (s[i] > 96 && s[i] < 123)	
+				s[i] -= 32;
+		}
+	}
+	return (s);
+}
