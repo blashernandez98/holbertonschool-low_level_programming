4531990874dfe3bb6fb3c70d3d1e43fd4893d44b
Blas <blashernandez98@gmail.com>
2022-06-17 08:43:24 -0700

    Task-2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - Function that locates char @c in string @s.
+ * @s: String to find in.
+ * @c: Char to look for.
+ * Return: Pointer to first occurance of @c in @s
+ * If no there is none, returns NULL.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (s)
+	{
+		if (*s == c)
+			break;
+		s++;
+	}
+	return (s);
+}
