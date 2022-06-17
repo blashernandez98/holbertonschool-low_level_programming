a4437065df551ce23575ab74a698f02e725b55f9
Blas <blashernandez98@gmail.com>
2022-06-17 08:19:31 -0700

    Task-0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - Functionality same as memset.
+ * @b: Byte to write in dest.
+ * @dest: Destination string to write byte @b.
+ * @n: N bytes to write @b in @dest
+ * Return: Pointer to result string.
+ */
+
+char *_memset(char *dest, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i]; i++)
+	{
+		dest[i] = b;
+	}
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
