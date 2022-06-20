7246bea6903b87f05a794d63940ef1d9b4970007
Blas <blashernandez98@gmail.com>
2022-06-20 09:03:39 -0700

    Task-1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Function that prints a reversed string with recursion
+ * @s: String to print.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	char start = *s;
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
