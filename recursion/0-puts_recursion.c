4ee8995df1a72b4dd9f75e33828eead3ac76641b
Blas <blashernandez98@gmail.com>
2022-06-20 08:45:03 -0700

    Task-0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Similar function to puts but with recursion.
+ * @s: String to print.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*str);
+		_puts_recursion(str + 1);
+	}
+}
