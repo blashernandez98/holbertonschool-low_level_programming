66abd7f01fe589f6c1e2363980d9dfc141d7652a
Blas <blashernandez98@gmail.com>
2022-06-20 09:50:46 -0700

    Task-5

---
+++ recursion/5-sqrt_recurstion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * guess - Finds sqrt of int by guessing
+ * @n: Int to find sqrt.
+ * @g: Guess
+ * Return: Sqrt of @n if exists, -1 otherwise.
+ */
+
+int guess(int n, int g)
+{
+	if (g > 0)
+	{
+		if (n / g == g)
+			return (g);
+		else
+			return (guess(n, g - 1));
+	}
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - Find natural sqrt of int.
+ * @n: Int to find sqrt.
+ * Return: Sqrt of @n if it exists, -1 otherwise.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n > 3)
+		return (guess(n, n/2));
+	return (-1);
+}
