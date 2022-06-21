779d53d006a4cb34b509d11c9028d44636695402
Blas <blashernandez98@gmail.com>
2022-06-21 14:45:18 -0700

    Task-1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Main function of program. Entry point.
+ * @argc: Number of arguments passed to main.
+ * @argv: Pointer to array of strings of arguments.
+ * Return: 0 always.
+ */
+
+int main(int argc, __attribute__ ((unused)) char **argv)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
