67e73310a83f7a1d274382ed9c400c1ef5178038
Blas <blashernandez98@gmail.com>
2022-06-22 07:07:58 -0700

    Task-100

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * rot13 - Encodes string using rot13.
+ * @s: String to encode.
+ * Return: Pointer to encoded string.
+ */
+
+char *rot13(char *s)
+{
+	char alpha[53];
+	char rot_13[53];
+	int i, j;
+	
+	alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	rot_13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; alpha[j]; j++)
+		{
+			if (aplha[j] == s[i])
+			{
+				s[i] = rot_13[j];
+				break;
+			}
+		}	
+	}
+	return (s);
+}
