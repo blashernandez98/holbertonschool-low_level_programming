1a34eb475b271009efa5be7dbc2874ded6f4981b
Blas <blashernandez98@gmail.com>
2022-06-23 11:52:35 -0700

    Task-1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Creates duplicate of string
+ * @str: String to duplicate
+ * Return: Pointer to new memory with copy of str or NULL if fails.
+ */
+
+char *_strdup(char *str)
+{
+	char *copy = NULL;
+	
+	if (str)
+		copy = malloc(strlen(str));
+	if (copy)
+	{
+		for (i = 0; i < strlen(str); i++)
+			copy[i] = str[i];
+	}
+	return (copy);
+}
