c75a73722d0d0c4d06101999df9507e366b22942
Blas <blashernandez98@gmail.com>
2022-06-26 09:47:02 -0700

    Task-100

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - Concatenates all arguments of your program.
+ * @ac: Number of arguments.
+ * @av: Pointer to array of arguments.
+ * Return: Pointer to new string of concat args.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str = NULL;	
+	int i, j, k, size = 0;
+
+	if (ac && av)
+	{
+		for (i = 0; i < ac; i++)
+			size += strlen(av[i]);
+		str = malloc(sizeof(char) * (size + ac + 1));
+		if (str)
+		{
+			i = 0;
+			for (j = 0; j < ac; j++, i++)
+			{
+				for (k = 0; av[j][k]; i++, k++)
+					str[i] = av[j][k];
+				str[i] = '\n';
+			}
+			str[i] = '\0';
+		}
+	}
+	return (str);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -2,3 +2,6 @@ void _putchar(char);
 char *create_array(unsigned int, char);
 char *_strdup(char *);
 char *str_concat(char *, char *);
+int **alloc_grid(int, int);
+void free_grid(int **, int);
+char *argstostr(int, char **);
