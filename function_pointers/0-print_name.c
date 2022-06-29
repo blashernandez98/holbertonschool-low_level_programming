5a842c3ff102b09a03d54b0f1824a3aee0cda4d1
Blas <blashernandez98@gmail.com>
2022-06-29 16:49:14 -0700

    Task-0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name: Function that prints a name.
+ * @name: Name to print.
+ * @f: Pointer to function that prints.
+ */
+
+void print_name(char *name, void (*f)(char *str))
+{
+	*f(str);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+void print_name(char *, void (*)(char *));
+#endif
