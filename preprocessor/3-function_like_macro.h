bd0ff4536c79661be89ca6712893063886dac5fd
Blas <blashernandez98@gmail.com>
2022-06-29 11:00:45 -0700

    Task-3

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef "3-functions_like_macro.h"
-#define "3-functions-like_macro.h"
+#ifndef FUNCTIONS_LIKE_MACROS
+#define FUNCTIONS_LIKE_MACROS
 #define ABS(X) (((X) < (0)) ? -(X) : (X))
 #endif
