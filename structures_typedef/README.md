5ef00d4dbb7b5a9cfb12e9c24dfbe4587a93a294
Blas <blashernandez98@gmail.com>
2022-06-29 12:53:30 -0700

    Task-0

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+# This directory contains the tasks for the "C - Structures, typedef" project.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,9 @@
+#ifndef DOG_H
+#define DOG_H
+typedef struct Dog
+{
+	char *name;
+	char *owner;
+	float age;
+} dog;
+#endif
