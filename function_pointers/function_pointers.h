7256d0b64fdfb641d3681e6f6f6cec26a435e55a
Blas <blashernandez98@gmail.com>
2022-06-29 18:19:35 -0700

    header

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 void print_name(char *, void (*)(char *));
-void array_iterator(int *, int, (*)(int));
+void array_iterator(int *, int, void (*)(int));
 #endif
