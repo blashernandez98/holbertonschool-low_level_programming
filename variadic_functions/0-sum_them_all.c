42ff640e34f43811491ad87d0406bfd91885f5a1
Blas <blashernandez98@gmail.com>
2022-07-01 08:30:06 -0700

    Task-0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,12 +12,10 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list arg_ptr;
-	int sum = n;
+	int sum = 0;
 
 	va_start(arg_ptr, NULL);
-	if (n == 0)
-		return (0);
-	while (arg_ptr)
+	for (i = 0; i < n, i++)
 		sum += va_arg(arg_ptr, int);
 	return (sum);
 }
