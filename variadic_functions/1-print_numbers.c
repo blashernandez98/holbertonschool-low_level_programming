fb3cd6379364bc6bcfbb84f1ef10fd5e2fecccd7
Blas <blashernandez98@gmail.com>
2022-07-01 08:56:41 -0700

    Task-1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,15 +18,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(arg_ptr, NULL);
 
-	if (n == 0)
-		return;
-	if (!separator)
-		s = "";
-	else
-		s = strdup(separator);
-	for (i = 0; i < n - 1; i++)
+	if (n)
 	{
-		printf("%d%s", va_arg(arg_ptr, int), s);
+		if (!separator)
+			s = "";
+		else
+			s = strdup(separator);
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d%s", va_arg(arg_ptr, int), s);
+		}
+		printf("%d", va_arg(arg_ptr, int));
 	}
-	printf("%d\n", va_arg(arg_ptr, int));
+	printf("\n");
 }
