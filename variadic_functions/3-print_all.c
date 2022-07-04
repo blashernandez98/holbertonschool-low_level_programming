4547d634266a5c27ce0efaafb2dda1cb45587f26
Blas <blashernandez98@gmail.com>
2022-07-04 11:33:30 -0700

    Task-3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,35 +5,14 @@
 #include <string.h>
 
 /**
- * after_format - Deals with everything after a succesful print.
- * @args_ptr: Pointer to int args.
+ * after_print - Deals with everything after a succesful print.
+ * @format_ptr: Pointer to format string.
  */
 
-void after_format(int *args_ptr)
+void after_print(const char * const format_ptr)
 {
-	*args_ptr = *args_ptr - 1;
-	if (*args_ptr)
+	if (strpbrk(format_ptr + 1, "cifs"))
 		printf(", ");
-}
-
-/**
- * count_format - Counts number of chars that match format.
- * @s: String passed as format.
- * Return: Number of correct format chars.
- */
-
-int count_format(const char * const s)
-{
-	int i = 0, res = 0;
-	const char * const format = "cifs";
-
-	while (s[i])
-	{
-		if (strchr(format, s[i]))
-			res++;
-		i++;
-	}
-	return (res);
 }
 
 /**
@@ -44,30 +23,37 @@ int count_format(const char * const s)
 void print_all(const char * const format, ...)
 {
 	va_list arg_ptr;
-	int i = 0, args;
+	char *sarg;
+	int i = 0;
 
 	va_start(arg_ptr, 0);
-	args = count_format(format);
 
-	while (format[i])
+	while (format && format[i])
 	{
 		switch (format[i])
 		{
 			case 'c':
 				printf("%c", va_arg(arg_ptr, int));
-				after_format(&args);
+				after_print(format + i);
 				break;
 			case 'i':
 				printf("%d", va_arg(arg_ptr, int));
-				after_format(&args);
+				after_print(format + i);
 				break;
 			case 's':
-				printf("%s", va_arg(arg_ptr, char *));
-				after_format(&args);
+				sarg = va_arg(arg_ptr, char *);
+				if (sarg)
+				{
+					printf("%s", sarg);
+					after_print(format + i);
+					break;
+				}
+				printf("(nil)");
+				after_print(format + i);
 				break;
 			case 'f':
 				printf("%f", va_arg(arg_ptr, double));
-				after_format(&args);
+				after_print(format + i);
 				break;
 			default:
 				break;
