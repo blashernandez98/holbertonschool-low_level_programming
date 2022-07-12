6a476f3f98350172df2473f85f5a7c2f6a542589
Blas <blashernandez98@gmail.com>
2022-07-12 08:05:04 -0700

    Task0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,9 +15,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str)
-			printf("[%d] %s", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		else
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		res++;
 		h = h->next;
 	}
