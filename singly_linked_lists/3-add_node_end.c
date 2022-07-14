6666e026ac85ea8231501768c09086a251040794
Blas <blashernandez98@gmail.com>
2022-07-14 10:26:29 -0700

    Task3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * add_node_end - Adds node at end of linked list.
@@ -22,9 +23,9 @@ list_t *add_node_end(list_t **head, const char *str)
 			new->str = strd;
 			new->len = strlen(strd);
 			new->next = NULL;
-			while (*head->next)
-				*head = *head->next;
-			*head->next = new;
+			while ((*head)->next)
+				*head = (*head)->next;
+			(*head)->next = new;
 		}
 		else
 			free(strd);
