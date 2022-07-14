303be492d1c652668e3ee2dd1bbe2d91a59ba5c8
Blas <blashernandez98@gmail.com>
2022-07-14 11:18:27 -0700

    Task4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 
 /**
+ * free_node - Frees memory for node of list_t
+ * @node: Pointer to node.
+ */
+
+void free_node(list_t *node)
+{
+	if (node->str)
+		free(node->str);
+	free(node);
+}
+
+
+/**
  * free_list - Frees memory allocated for a list.
  * @head: Pointer to first element.
  */
@@ -13,9 +26,9 @@ void free_list(list_t *head)
 	while (head && head->next)
 	{
 		cursor = head->next;
-		free(head);
+		free_node(head);
 		head = cursor;
 	}
 	if (head)
-		free(head);
+		free_node(head);
 }
