b31c67f9581a6e5a9e3e5e337748f5b49a5fbc6c
Blas <blashernandez98@gmail.com>
2022-07-14 14:21:47 -0700

    Task3

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		new->n = n;
 		new->next = NULL;
-		while (cursor->next)
+		while (cursor && cursor->next)
 			cursor = cursor->next;
 		cursor->next = new;	
 	}
