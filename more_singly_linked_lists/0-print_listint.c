4574cb46884acae3fd02437e596e6782db25fc84
Blas <blashernandez98@gmail.com>
2022-07-14 11:29:22 -0700

    Task0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - Prints list of ints.
+ * @head: Pointer to first node.
+ * 
+ * Return: Size of list.
+ */
+
+size_t print_listint(const listint_t *head)
+{
+	size_t size = 0;
+
+	while (head)
+	{
+		printf("%d\n", head->n);
+		size++;
+		head = head->next;
+	}
+	printf("-> %d elements\n", size);
+	return (size);
+}
---
+++ more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+# This folder contains all the tasks for the "C - More singly linked lists" project at Holberton School.
