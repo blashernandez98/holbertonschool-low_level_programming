993120f982cdb53d576eb42b8674d60e56a01b4c
Blas <blashernandez98@gmail.com>
2022-07-14 10:06:09 -0700

    Task2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -5,12 +5,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
-	if (new)
+	char *strd = strdup(str);
+
+	if (new && strd)
 	{
-		new->str = strdup(str);
+		new->str = strd;
 		new->len = strlen(new->str);
 		new->next = *head;
 		*head = new;
 	}
+	else if (!strd && new)
+		free(new);
+	else if (!new && strd)
+		free(strd);
 	return (new);
 }
