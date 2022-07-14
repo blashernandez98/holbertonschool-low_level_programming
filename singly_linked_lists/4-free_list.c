1fc1975be81ee76b8fdfe13e12093caa4aaee516
Blas <blashernandez98@gmail.com>
2022-07-14 11:10:27 -0700

    Task4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Frees memory allocated for a list.
+ * @head: Pointer to first element.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *cursor = head;
+
+	while (head->next)
+	{
+		cursor = head->next;
+		free(head);
+		head = cursor;
+	}
+	free(head);
+}
