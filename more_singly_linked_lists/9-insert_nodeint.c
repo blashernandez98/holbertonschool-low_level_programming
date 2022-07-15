a5aceabf1f52ef58552f624c10e6fea68b42de21
Blas <blashernandez98@gmail.com>
2022-07-15 12:43:34 -0700

    Task9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
 	unsigned int i;
 	size_t len;
-	listint_t *cursor, new;
+	listint_t *cursor, *new;
 
 	if (!head)
 		return (NULL);
