2bfbea5d3cdc55f1fbd08f095b2f3d13647b4167
Blas <blashernandez98@gmail.com>
2022-07-14 20:13:10 -0700

    Task4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - Frees listint.
+ * @head: Pointer to first element of list.
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *aux = head;
+	while (head && head->next)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+	if (head && !(head->next))
+		free(head);
+}
