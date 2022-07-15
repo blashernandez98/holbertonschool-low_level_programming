8d385cd41b8dbdc69df557baffb17d2dee57c33c
Blas <blashernandez98@gmail.com>
2022-07-15 12:41:56 -0700

    Task9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Insert node at nth index of intlist.
+ * @head: Pointer to head pointer element.
+ * @index: Index to add in.
+ * @n: Int for new node.
+ *
+ * Return: Pointer to new node.
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+{
+	unsigned int i;
+	size_t len;
+	listint_t *cursor, new;
+
+	if (!head)
+		return (NULL);
+	len = listint_len(*head);
+	if (len < index)
+		return (NULL);
+
+	if (index == 0)
+		return (add_nodeint(head, n));
+	
+	cursor = *head;
+	for (i = 0; i != index - 1; i++)
+		cursor = cursor->next;
+
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	new->next = cursor->next;
+	cursor->next = new;
+	return (new);
+}
