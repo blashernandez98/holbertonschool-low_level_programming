ad30aafffb9c7cd41c564dda059bbaccd3b6eb63
Blas <blashernandez98@gmail.com>
2022-07-20 11:13:22 -0700

    Task4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - Sets bit of n at index to 0.
+ * @n: Number
+ * @index: Index
+ *
+ * Return: 1 if successful, -1 if fails.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 31)
+		return (-1);
+
+	*n |= (0 >> index);
+
+	return (1);
+}
