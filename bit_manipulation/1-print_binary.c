e420594037511804157b48c26099ad95e8c28a11
Blas <blashernandez98@gmail.com>
2022-07-20 10:08:11 -0700

    Task1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * recursive_print - Prints non 0 number in binary
+ * @n: Number to print
+ */
+
+void recursive_print(unsigned long int n)
+{
+	if (n)
+	{
+		recursive_print(n >> 1);
+		printf("%d", n & 1);
+	}
+}
+
+/**
+ * print_binary - Prints number in binary
+ * @n: Number to print
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n)
+		recursive_print(n);
+	else
+		printf("0");
+}
