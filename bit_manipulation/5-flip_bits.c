cc99efae8bb67f839fc6e53b002c098d46f8bdc2
Blas <blashernandez98@gmail.com>
2022-07-20 11:44:28 -0700

    Task4

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * recursive_bits - Finds number of different bits in number.
- * @n: First number
- * @m: Second number
- *
- * Return: 1 if current bit different, 0 otherwise.
- */
-
-int recursive_bits(unsigned long int n, unsigned long int m)
-{
-	int res = 0;
-
-	if (n || m)
-	{
-		res += recursive_bits(n >> 1, m >> 1);
-		return ((~((n & 1) & (m & 1))) + res);
-	}
-	return (res);
-}
-
-/**
  * flip_bits - returns the number of bits you would need to flip
  * to get from one number to another.
  * @n: First number
@@ -33,11 +13,12 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int res = 0;
 
-	while (n || m)
+	n = n ^ m;
+
+	while (n)
 	{
-		res += (~((n & 1) & (m & 1)));
+		res += n & 1;
 		n >>= 1;
-		m >>= 1;
 	}
 	return (res);
 }
