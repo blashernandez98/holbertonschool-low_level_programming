72a699ed2546eb36e4ab1637dc8c4c4902670940
Blas <blashernandez98@gmail.com>
2022-07-20 10:25:01 -0700

    Task2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - Get bit at index
+ * @n: Number
+ * @index: Index
+ *
+ * Return: Bit of n at index.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index)
+		return ((n >> index) & 1);
+	return (n && 1);
+}
