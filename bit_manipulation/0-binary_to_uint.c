2d8c0a82103bc06413ef8c45300b39982965ee23
Blas <blashernandez98@gmail.com>
2022-07-20 08:34:27 -0700

    Task0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,28 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * pow2 - Calculates powers of 2.
+ * @power: Power to calculate.
+ *
+ * Return: 2 ^ @power
+ */
+
 int pow2(int power)
 {
 	int i = 0, res = 1;
-	
+
 	for (i = 0; i < power; i++)
 		res *= 2;
 	return (res);
 }
+
+/**
+ * binary_to_uint - Converts binary to unsigned int
+ * @b: String of 0s and 1s.
+ *
+ * Return: Converted number or 0 if fails.
+ */
 
 unsigned int binary_to_uint(const char *b)
 {
