aad9d4650bb0c313252e6e52b0c85c0e25356a58
Blas <blashernandez98@gmail.com>
2022-07-20 18:04:20 -0700

    Task1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * read_textfile - Creates new file.
+ * @filename: Name of new file.
+ * @text_content: Content for new file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	fd = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+	
+	letters = write(fd, text_content, strlen(text_content));
+
+	if (letters == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
