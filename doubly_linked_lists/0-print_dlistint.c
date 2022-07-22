beed7d694b7827cbfee48613b4ffde306cfc25ee
Blas <blashernandez98@gmail.com>
2022-07-22 15:17:29 -0700

    Task0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - Function that prints all the elements of a dlistint_t list.
+ * @h: Pointer to first node of list.
+ *
+ * Return: Number of elements printed.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t res = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		res++;
+	}
+
+	return (res);
+}
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+# This folder contains the tasks for the "C - Doubly liked lists" project at Holberton School.
