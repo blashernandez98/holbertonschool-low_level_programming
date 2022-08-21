7a9525d945ab6b2bcdd686835da0185337273f84
Blas <blashernandez98@gmail.com>
2022-08-21 09:58:43 -0700

    Task3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,19 +13,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t **location = NULL, *new = NULL;
-
+	char *key_cp, *value_cp;
+	
 	if (ht && key)
 	{
-		index = key_index(key, ht->size);
+		key_cp = strdup(key);
+		if (!(key_cp))
+			return (0);
+		value_cp = strdup(value);
+		if (!(value_cp))
+			return (0);
+
+		index = key_index((unsigned char *) key_cp, ht->size);
 		location = (ht->array);
 		location += index;
+		
 		new = malloc(sizeof(hash_node_t));
 		if (!new)
 			return (0);
-		new->key = strdup(key);
-		new->value = strdup(value);
-		if (!(new->value) || !(new->key))
-			return (0);
+		new->key = key_cp;
+		new->value = value_cp;
+		new->next = NULL;
 
 		if (*location != NULL)
 		{
