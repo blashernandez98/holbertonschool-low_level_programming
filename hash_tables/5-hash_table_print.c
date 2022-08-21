7049b0f460334a580ebbf79fdb663fc705a5cb10
Blas <blashernandez98@gmail.com>
2022-08-21 11:25:08 -0700

    Task5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,6 +10,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t **cursor = NULL, *list = NULL;
 	unsigned long int i;
+	int first = 0;
 
 	printf("{");
 	if (ht)
@@ -21,9 +22,10 @@ void hash_table_print(const hash_table_t *ht)
 			list = cursor[i];
 			while (list)
 			{
-				printf("'%s': '%s'", list->key, list->value);
-				if (list->next)
+				if (first)
 					printf(", ");
+				first = 1;
+				printf("'%s': '%s'", list->key, list->value);
 				list = list->next;
 			}
 		}
