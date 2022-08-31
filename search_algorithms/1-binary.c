d6ab0f3fb9827b9d2df05b5afd100497218aa206
Blas <blashernandez98@gmail.com>
2022-08-30 17:19:31 -0700

    Task1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,6 +1,22 @@
 #include "search_algos.h"
 
 /**
+ * print_array - Prints array.
+ * @a: Array
+ * @size: Size of array
+ */
+
+void print_array(int *a, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = 0; i < size - 1; i++)
+		printf("%d, ", a[i]);
+	printf("%d\n", a[i]);
+}
+
+/**
  * binary_search - Searches for a value in an array.
  * @array: Input array.
  * @size: Size of array.
@@ -11,25 +27,29 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int half;
+	int half, pos;
 	size_t i;
 
 	if (array && size > 1)
 	{
 		half = (size - 1) / 2;
-		printf("Searching in array: ");
-		for (i = 0; i < size - 1; i++)
-		{
-			printf("%d, ", array[i]);
-		}
-		printf("%d\n", array[i]);
+		print_array(array, size);	
 		if (array[half] == value)
 			return (half);
 		else if (array[half] > value)
 			return (binary_search(array, half, value));
 		else
-			return (binary_search(array + half + 1, size - half - 1, value));
+		{
+			pos = binary_search(array + half + 1, size - half - 1, value);
+			if (pos != -1)
+				pos += half + 1;
+			return (pos);
+		}
 	}
-	else
-		return (-1);
+	else if (size == 1)
+	{
+		if (array[0] == value)
+			return (0);
+	}
+	return (-1);
 }
